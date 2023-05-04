4ff3c8143949676f20ee043362ea25bd76911c2d
barath-hike <barath@hike.in>
2023-05-04 08:15:58 +0000

    Use all zk endpoints

--- utils/config_utils.py
+++ utils/config_utils.py
@@ -4,12 +4,17 @@ config_path = './config.json'
 slack_url_path = './slack_url.json'
 
 def load_config():
+
     with open(config_path, 'r') as f:
         return json.load(f)
 
 def load_slack_config():
+
     try:
+
         with open(slack_url_path, 'r') as f:
             return json.load(f)['slack_url']
+
     except:
+        
         return ''
--- utils/zk_utils.py
+++ utils/zk_utils.py
@@ -10,12 +10,46 @@ def zk_connection():
 
     config = load_config()
 
-    host = config[config['env']]['zk']['host']
+    zk = []
 
-    zk = KazooClient(hosts=host)  # Replace with your Zookeeper connection string
-    zk.start()  # Establish the connection
+    for host in config[config['env']]['zk']['host']:
+
+        zk_conn = KazooClient(hosts=host)  # Replace with your Zookeeper connection string
+        zk_conn.start()  # Establish the connection
+
+        zk.append(zk_conn)
 
     return zk
+
+def path_exists_in_zk(zk_list, path):
+
+    for zk_conn in zk_list:
+
+        try:
+
+            if zk_conn.exists(path):
+                return True
+
+        except:
+
+            continue
+
+    return False
+
+def set_zknode(zk, path, json_data):
+
+    for zk_conn in zk:
+
+        try:
+
+            zk_conn.set(path, json_data)
+            return True
+
+        except:
+
+            continue
+
+    return False
 
 def update_znode(zk, game, znode_path, new_data, default_data, slack_url):
 
@@ -28,39 +62,43 @@ def update_znode(zk, game, znode_path, new_data, default_data, slack_url):
         path = znode_path + game
 
     try:
-        json_data = json.dumps(new_data, indent=4)
-        
-        if zk.exists(path):
-            zk.set(path, json_data.encode('utf-8'))
-            message = f"Znode '{path}' updated with new data: {json_data}"
 
+        json_data = json.dumps(new_data, indent=4).encode('utf-8')
+
+        if path_exists_in_zk(zk, path):
+
+            if not set_zknode(zk, path, json_data):
+                raise Exception("Failed to update znode with new data")
+
+            message = f"Znode '{path}' updated with new data: {json_data}"
             updated_window = new_data.copy()
 
         else:
             message = f"Znode '{path}' not found"
-
             updated_window = default_data.copy()
 
     except:
 
         try:
-            json_data = json.dumps(default_data, indent=4)
-            
-            if zk.exists(path):
-                zk.set(path, json_data.encode('utf-8'))
-                message = f"Znode '{path}' updated with new data: {json_data}"
 
+            json_data = json.dumps(default_data, indent=4).encode('utf-8')
+
+            if path_exists_in_zk(zk, path):
+
+                if not set_zknode(zk, path, json_data):
+                    raise Exception("Failed to update znode with default data")
+
+                message = f"Znode '{path}' updated with new data: {json_data}"
                 updated_window = default_data.copy()
 
             else:
-                message = f"Znode '{path}' not found"
 
+                message = f"Znode '{path}' not found"
                 updated_window = default_data.copy()
 
         except Exception as e:
 
             message = f"Error updating znode '{path}': {e}"
-
             updated_window = default_data.copy()
 
     print(message)
