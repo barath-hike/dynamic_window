d802dea38add65679fa4d517b5202c82f8b9ed3b
barath-hike <barath@hike.in>
2023-05-22 12:00:18 +0000

    kafka consumer

---
+++ liquidity_consumer.py
@@ -0,0 +1,26 @@
+from utils.config_utils import load_config
+from utils.kafka_utils import get_consumer, DataAggregator
+
+consumer = get_consumer()
+
+config = load_config()
+
+table_ids = config['table_ids']
+aggregators = {table_id:DataAggregator(table_id) for table_id in table_ids}
+
+for message in consumer:
+
+    value = message.value
+
+    if 'o' in value and 'k' in value and value['k'] == 'rush' and value['o'] == 'table_search_started' and 'f' in value and 'fu' in value and 's' in value:
+        
+        table_id = value['s']
+
+        if table_id in table_ids:
+
+            userid = value['fu']
+            mmid = value['f']
+
+            record = {'userid': userid, 'mmid': mmid}
+
+            data_agg = aggregators[table_id].add(record)
---
+++ utils/kafka_utils.py
@@ -0,0 +1,74 @@
+from kafka import KafkaConsumer
+from json import loads
+import pickle
+import pytz
+import pandas as pd
+from datetime import datetime
+
+from utils.config_utils import load_config
+
+def get_consumer():
+
+    config = load_config()
+
+    hosts = config[config['env']]['kafka']['hosts']
+    topic_name = config[config['env']]['kafka']['topic_name']
+    group_id = config[config['env']]['kafka']['group_id']
+
+    consumer = KafkaConsumer(*topic_name, bootstrap_servers=hosts, enable_auto_commit=True, group_id=group_id, value_deserializer=lambda x: loads(x))
+
+    return consumer
+
+class DataAggregator:
+
+    def __init__(self, pre):
+
+        self.pre = pre
+        
+        self.ist = pytz.timezone('Asia/Kolkata')
+        current_time_ist = datetime.now(self.ist)
+
+        self.current_time_rounded = self.round_time_to_ten_minutes(current_time_ist)
+        self.start_time = self.current_time_rounded
+
+        self.data = []
+        self.counter = self.calculate_counter(self.current_time_rounded)
+
+    @staticmethod
+    def round_time_to_ten_minutes(dt):
+        minute = (dt.minute // 10) * 10
+        return dt.replace(minute=minute, second=0, microsecond=0)
+
+    @staticmethod
+    def calculate_counter(dt):
+        return dt.hour * 6 + dt.minute // 10 + 1
+
+    def add(self, record):
+        
+        current_time_ist = datetime.now(self.ist)
+        current_time_rounded = self.round_time_to_ten_minutes(current_time_ist)
+        if current_time_rounded > self.current_time_rounded:
+            self.dump_data()
+            self.reset(current_time_rounded)
+
+        self.data.append(record)
+
+    def dump_data(self):
+
+        data = pd.DataFrame(self.data)
+        out = {
+            'mm_starts': len(data[['userid', 'mmid']].drop_duplicates()),
+            'num_users': len(data['userid'].unique())
+        }
+
+        current_time_ist = datetime.now(self.ist)
+        current_time_ist = current_time_ist.strftime('%Y-%m-%d')
+
+        with open(f'../data/{self.pre}_{current_time_ist}_{self.counter}.pickle', 'wb') as f:
+            pickle.dump(out, f)
+
+    def reset(self, new_start_time):
+        
+        self.current_time_rounded = new_start_time
+        self.data = []
+        self.counter = self.calculate_counter(self.current_time_rounded)
